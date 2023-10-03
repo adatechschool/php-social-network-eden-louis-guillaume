82bc8c73e484876d07c3a63ba739f79b2d40f80b
Guillaume <135039493+Guy0851@users.noreply.github.com>
2023-10-03 14:13:20 +0200

    ajout de commentaires

--- resoc_n1/wall.php
+++ resoc_n1/wall.php
@@ -94,26 +94,8 @@
 
                     // echo "<pre>" . print_r($post, 1) . "</pre>";
                     ?>                
-                    <article>
-                        <h3>
-                            <time datetime='' ><?php echo $post['created'] ?></time>
-                        </h3>
-                        <address>par <?php echo $post['author_name'] ?></address>
-                        <div>
-                            <p><?php echo $post['content'] ?></p>
-
-                        </div>                                            
-                        <footer>
-                            <small>♥ <?php echo $post['like_number'] ?></small>
-                            <?php $str = $post['taglist'];
-                            $delimiter = ",";
-                            $parts = explode($delimiter, $str);
-                            for ($i=0; $i<count($parts); $i++) {
-                                echo '<a href="#">#' . $parts[$i] . ' ' . '</a>';
-                            } ?>
-                        </footer>
-                    </article>
-                <?php } ?>
+                   
+                <?php include "post.php"; } ?>
 
 
             </main>
